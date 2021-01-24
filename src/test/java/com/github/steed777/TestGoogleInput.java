712058f37df77b60334a5d725750999824ce8366
steed777 <steed777@yandex.ru>
2021-01-25 01:14:10 +0300

    Initial commit

---
+++ src/test/java/com/github/steed777/TestGoogleInput.java
@@ -0,0 +1,44 @@
+package com.github.steed777;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TestGoogleInput {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private WebElement element;
+
+    @Before
+    public void startDriver() {
+        FirefoxOptions options = new FirefoxOptions();
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver(options);
+        wait = new WebDriverWait(driver, 10);
+    }
+    @Test
+    public void testgoogleinput()  {
+        try {
+            driver.get("https://www.google.ru/");
+            element = driver.findElement(By.cssSelector("[name=\"q\"]"));
+            element.sendKeys("купить кофемашину bork c804");
+            element.click();
+            element.sendKeys(Keys.ENTER);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("_H-0NYLCBCOvzqwHflIjoBg1")));
+        }catch (Exception e) {
+        }
+    }
+    @After
+    public void stopDriver(){
+        driver.quit();
+    }
+}

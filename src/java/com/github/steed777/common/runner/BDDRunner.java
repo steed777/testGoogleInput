d5f8c248674e88f1c8779aa5ab06f2d41a4908a2
Senko Konstantin <steed777@yandex.ru>
2022-04-18 16:26:33 +0300

    Редактирование

--- src/java/com/github/steed777/common/runner/BDDRunner.java
+++ src/java/com/github/steed777/common/runner/BDDRunner.java
@@ -5,22 +5,21 @@ import com.github.steed777.common.Application;
 import com.github.steed777.common.Browser;
 
 
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.AfterClass;
-import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
-@CucumberOptions (
+
+@CucumberOptions(
         plugin = {
                 "pretty",
               //  "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
                // "json:target/cucumber.json"
         },
         features = "src/test/resources/features/",
+       // allure-results = "target",
         glue = "com.github.steed777.steps")
-public class BDDRunner{
+public class BDDRunner extends AbstractTestNGCucumberTests {
 
 
     /*@AfterClass
